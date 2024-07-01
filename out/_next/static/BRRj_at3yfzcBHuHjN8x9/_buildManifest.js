74780aa06a741f0a0225a620b811066dda69e157
Jack Telford <wipke9893@yahoo.com>
2024-07-01 15:40:26 -0400

    out

---
+++ out/_next/static/BRRj_at3yfzcBHuHjN8x9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/4110-2921971c4e837e3f.js","static/chunks/4162-aff352c28ba125c3.js","static/chunks/pages/index-13ddc0e1d5ecbe55.js"],"/_error":["static/chunks/pages/_error-a40d28c628e70b05.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
